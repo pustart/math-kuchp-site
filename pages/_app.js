4979e7cb6b0300cccccf2e9d570b248a559f3e57
pustart <polaris9955@gmail.com>
2023-04-17 16:05:07 +0300

    feat: add all types of favicon

--- pages/_app.js
+++ pages/_app.js
@@ -7,6 +7,8 @@ export default function MyApp({ Component, pageProps }) {
     <Head>
       <title>Кафедра уравнений в частных производных и теории вероятности | Кафедра математического факультета ВГУ</title>
       <link rel="icon" href="/favicon.ico" />
+      <link rel="apple-icon" href="/images/favicons/apple-icon.png" />
+      <link rel="manifest" href="/manifest.webmanifest" />
     </Head>
     <Component {...pageProps} />
   </>;
