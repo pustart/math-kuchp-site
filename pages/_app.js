683ba096b12837822daea99b4786c25c174e470b
pustart <polaris9955@gmail.com>
2023-04-17 13:39:56 +0300

    init: start next v13 project

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,13 @@
-import '@/styles/globals.css'
+import '../styles/globals.css';
+import Head from 'next/head';
+import React from 'react';
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default function MyApp({ Component, pageProps }) {
+  return <>
+    <Head>
+      <title>Кафедра уравнений в частных производных и теории вероятности | Кафедра математического факультета ВГУ</title>
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+    <Component {...pageProps} />
+  </>;
 }
