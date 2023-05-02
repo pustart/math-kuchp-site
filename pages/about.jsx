5073c202aa5ec8b03140ec42e591e5e5af70132b
pustart <polaris9955@gmail.com>
2023-05-02 18:03:49 +0300

    feat: add contacts props for footer

--- pages/about.jsx
+++ pages/about.jsx
@@ -1,15 +1,29 @@
 import React from "react";
 import Navbar from "../components/Navbar/Navbar";
 import CustomFooter from "../components/Footer/CustomFooter";
+import { fetchAPI } from "../lib/api";
 
-function News() {
+function News({ contacts }) {
   return (
     <div>
       <Navbar />
       <div>О кафедре</div>
-      <CustomFooter />
+      <CustomFooter contacts={contacts} />
     </div>
   );
+}
+
+export async function getStaticProps() {
+  const contacts = await fetchAPI("contact", {
+    fields: ["email", "general_number", "dean_number", "address"],
+  });
+
+  return {
+    props: {
+      contacts: contacts.data,
+    },
+    revalidate: 1,
+  };
 }
 
 export default News;
