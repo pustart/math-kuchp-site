37c9940fe6c32a092d6c00260c61f4574abc58df
pustart <polaris9955@gmail.com>
2023-06-09 00:04:03 +0300

    build: update config files

--- next.config.js
+++ next.config.js
@@ -1,9 +1,15 @@
 /** @type {import('next').NextConfig} */
+// const dns = require("dns");
+
+// dns.setDefaultResultOrder("ipv4first");
 const nextConfig = {
   reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   images: {
-    loader: "default",
-    domains: ["localhost", "math.vsu.ru"],
+    loader: 'default',
+    domains: ['localhost', 'math.vsu.ru', 'www.kuchp.ru'],
   },
 };
 
